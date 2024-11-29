5686672bcc304e46325c8f0eea0ebda0013c3ddb
kanaka <kanaka.grandhi@gmail.com>
2024-11-28 19:22:31 -0600

    first commit

---
+++ src/test/java/Arithmeticcalu/Addition.java
@@ -0,0 +1,17 @@
+package Arithmeticcalu;
+
+public class Addition {
+	
+	public static void main(String[] args) {
+		
+		int a = 8;
+		int b=9;
+		int c;
+		c=a+b;
+		
+		System.out.println("The sum is  c :"+c);
+		
+		
+	}
+
+}
